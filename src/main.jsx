05f6b937bc3ac8b85dc73d7f49c95afc929eea3b
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-07-11 11:11:52 +0530

    milk-status

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'
+const theme = createTheme();
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
